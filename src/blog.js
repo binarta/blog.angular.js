038088f647de84c0942ebd90ef84695ba795956e
Jo Kerkhofs <jke@thinkerit.be>
2014-04-15 15:11:19 +0200

    Changed default route

--- src/blog.js
+++ src/blog.js
@@ -1,7 +1,8 @@
 angular.module('blog', ['ngRoute', 'blog.controllers'])
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider
-            .when('/:locale/media/blog', {templateUrl:'partials/blog/index.html'});
+            .when('/blog', {templateUrl:'partials/blog/index.html'})
+            .when('/:locale/blog', {templateUrl:'partials/blog/index.html'});
     }]);
 
 angular.module('blog.controllers', ['blog.types'])
