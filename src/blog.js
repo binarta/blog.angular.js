2b4e241bba649561285d7551e009568f94c2ece4
Jo Kerkhofs <jke@thinkerit.be>
2014-08-18 15:37:21 +0200

    Added blog-template directive. Added blog templates.

--- src/blog.js
+++ src/blog.js
@@ -1,13 +1,34 @@
-angular.module('blog', ['ngRoute', 'blog.controllers'])
+angular.module('blog', ['ngRoute', 'blog.controllers', 'angularx'])
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider
             .when('/blog', {templateUrl:'partials/blog/index.html'})
-            .when('/:locale/blog', {templateUrl:'partials/blog/index.html'});
-    }]);
+            .when('/:locale/blog', {templateUrl:'partials/blog/index.html'})
+    }])
+    .directive('blogTemplate', ['binTemplate', BlogTemplateService]);
 
 angular.module('blog.controllers', ['blog.types'])
     .controller('AddBlogController', ['$scope', 'blogTypesLoader', AddBlogController]);
 
 function AddBlogController($scope, blogTypesLoader) {
     $scope.blogTypes = blogTypesLoader();
+}
+
+function BlogTemplateService(binTemplate) {
+    return {
+        scope: true,
+        restrict: 'A',
+        template: '<div ng-include="templateUrl"></div>',
+        link: function (scope, el, attrs) {
+            var permission;
+            if (attrs.blogTemplate == 'drafts') permission = 'blog.drafts.view';
+            if (attrs.blogTemplate == 'add') permission = 'catalog.item.add';
+
+            binTemplate.setTemplateUrl({
+                scope: scope,
+                module: 'blog',
+                name: attrs.blogTemplate + '.html',
+                permission: permission
+            });
+        }
+    };
 }
--- test/blog.spec.js
+++ test/blog.spec.js
@@ -2,6 +2,9 @@ describe('blog', function () {
     var scope, ctrl;
 
     beforeEach(module('blog.controllers'));
+    beforeEach(module('blog'));
+    angular.module('angularx', []);
+
     beforeEach(function () {
         scope = {};
     });
@@ -14,6 +17,86 @@ describe('blog', function () {
         it('should expose blog types on scope', inject(function (blogTypesLoader) {
             expect(scope.blogTypes).toEqual(blogTypesLoader());
         }));
+    });
+
+    describe('blog-template directive', function () {
+        var html, scope, directive, templateSpy, templateArgs;
+
+        beforeEach(inject(function ($rootScope) {
+            scope = $rootScope.$new();
+            templateArgs = {};
+            templateSpy = {
+                setTemplateUrl: function(args) {
+                    templateArgs = args;
+                }
+            };
+            directive = new BlogTemplateService(templateSpy);
+        }));
+
+        it('restricted to attribute', function () {
+            expect(directive.restrict).toEqual('A');
+        });
+
+        it('creates a child scope', function () {
+            expect(directive.scope).toEqual(true);
+        });
+
+        it('template', function () {
+            expect(directive.template).toEqual('<div ng-include="templateUrl"></div>');
+        });
+
+        describe('on link', function () {
+            describe('with template', function () {
+                beforeEach(function () {
+                    directive.link(scope, null, {
+                        blogTemplate: 'template'
+                    });
+                });
+
+                it('setTemplateUrl is called', function () {
+                    expect(templateArgs).toEqual({
+                        scope: scope,
+                        module: 'blog',
+                        name: 'template.html',
+                        permission: undefined
+                    });
+                });
+            });
+
+            describe('drafts', function () {
+                beforeEach(function () {
+                    directive.link(scope, null, {
+                        blogTemplate: 'drafts'
+                    });
+                });
+
+                it('setTemplateUrl is called', function () {
+                    expect(templateArgs).toEqual({
+                        scope: scope,
+                        module: 'blog',
+                        name: 'drafts.html',
+                        permission: 'blog.drafts.view'
+                    });
+                });
+            });
+
+            describe('add', function () {
+                beforeEach(function () {
+                    directive.link(scope, null, {
+                        blogTemplate: 'add'
+                    });
+                });
+
+                it('setTemplateUrl is called', function () {
+                    expect(templateArgs).toEqual({
+                        scope: scope,
+                        module: 'blog',
+                        name: 'add.html',
+                        permission: 'catalog.item.add'
+                    });
+                });
+            });
+        });
     });
 });
 
