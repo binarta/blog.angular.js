2decdba6621acce8b4c5c49f6f13691a39a316e2
Jo Kerkhofs <jke@thinkerit.be>
2015-08-29 12:55:09 +0200

    Added blog-unavailable templates

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,33 @@ var gulp = require('gulp'),
     minifyHtml = require('gulp-minify-html'),
     templateCache = require('gulp-angular-templatecache');
 
+var minifyHtmlOpts = {
+    empty: true,
+    cdata: true,
+    conditionals: true,
+    spare: true,
+    quotes: true
+};
+
 gulp.task('templates-bootstrap2', function () {
     gulp.src('template/bootstrap2/**/*.html')
-        .pipe(minifyHtml())
+        .pipe(minifyHtml(minifyHtmlOpts))
         .pipe(templateCache('blog-tpls-bootstrap2.js', {standalone: true, module: 'blog.templates'}))
         .pipe(gulp.dest('src'));
 });
 
 gulp.task('templates-bootstrap3', function () {
     gulp.src('template/bootstrap3/**/*.html')
-        .pipe(minifyHtml())
+        .pipe(minifyHtml(minifyHtmlOpts))
         .pipe(templateCache('blog-tpls-bootstrap3.js', {standalone: true, module: 'blog.templates'}))
         .pipe(gulp.dest('src'));
 });
 
-gulp.task('default', ['templates-bootstrap2', 'templates-bootstrap3']);
+gulp.task('unavailable-bootstrap3', function () {
+    gulp.src('template/bootstrap3/unavailable/*.html')
+        .pipe(minifyHtml(minifyHtmlOpts))
+        .pipe(templateCache('blog-unavailable-tpls-bootstrap3.js', {standalone: true, module: 'blog.templates'}))
+        .pipe(gulp.dest('src'));
+});
+
+gulp.task('default', ['templates-bootstrap2', 'templates-bootstrap3', 'unavailable-bootstrap3']);
