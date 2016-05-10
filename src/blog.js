c4af9fa3d28284a48ca8227034bcc5fb6b646005
Jo Kerkhofs <jke@thinkerit.be>
2016-05-10 15:03:12 +0200

    Removed deprecated blogTemplate directive

--- src/blog.js
+++ src/blog.js
@@ -10,7 +10,6 @@ angular.module('blog', ['ngRoute', 'blog.controllers', 'angularx', 'angularMomen
             .when('/blog/:blogType', {templateUrl:'partials/blog/index.html', controller: ['$scope', '$routeParams', BlogTypeController]})
             .when('/:locale/blog/:blogType', {templateUrl:'partials/blog/index.html', controller: ['$scope', '$routeParams', BlogTypeController]})
     }])
-    .directive('blogTemplate', ['binTemplate', BlogTemplateDirectiveFactory]);
 
 angular.module('blog.controllers', ['blog.types'])
     .controller('AddBlogController', ['$scope', 'blogTypesLoader', AddBlogController])
@@ -27,26 +26,4 @@ function BlogTypesController($scope, blogTypesLoader) {
 
 function BlogTypeController($scope, $routeParams) {
     $scope.blogType = $routeParams.blogType;
-}
-
-function BlogTemplateDirectiveFactory(binTemplate) {
-    return {
-        scope: true,
-        restrict: 'A',
-        template: '<div ng-include="templateUrl"></div>',
-        link: function (scope, el, attrs) {
-            if (attrs.settings) scope.settings = scope.$eval(attrs.settings);
-
-            var permission;
-            if (attrs.blogTemplate == 'drafts') permission = 'blog.drafts.view';
-            if (attrs.blogTemplate == 'add') permission = 'catalog.item.add';
-
-            binTemplate.setTemplateUrl({
-                scope: scope,
-                module: 'blog',
-                name: 'bin-blog-' + attrs.blogTemplate + '.html',
-                permission: permission
-            });
-        }
-    };
 }
